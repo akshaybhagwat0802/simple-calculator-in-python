4a5aba85b7f709cbb7f19337c99389b058fcbfa5
akshaybhagwat0802 <125860867+akshaybhagwat0802@users.noreply.github.com>
2025-02-05 12:22:27 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,21 @@
+# take input from user 
+x = input("Enter first number :")
+y = input("Enter second number :")
+print("the value of",x,"+",y,"is:",int(x)+int(y))
+print ("the value of",x,"-",y , "is" ,int (x)-int(y))
+print ("the value of",x,"*",y , "is" ,int (x)*int(y))
+print ("the value of",x,"/",y , "is" ,int (x)/int(y))
+print ("the value of",x,"//",y , "is" ,int (x)//int(y))
+print ("the value of",x,"**",y , "is" ,int (x)**int(y))
+print ("the value of",x,"%",y , "is" ,int (x)%int(y))
+
+# simple calculator
+a=12
+b=5
+print("the value of",a,"+", 5 ,"is:",a+b)
+print("the value of",a,"-", 5 ,"is:",a-b)
+print("the value of",a,"*", 5 ,"is:",a*b)
+print("the value of",a,"/", 5 ,"is:",a/b)
+print("the value of",a,"**", 5 ,"is:",a**b)
+print("the value of",a,"//", 5 ,"is:",a//b)
+print("the value of",a,"%", 5 ,"is:",a%b)
